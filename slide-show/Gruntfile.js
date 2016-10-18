efd017663f63f42165cb13b32c66c76c6ddc6d4c
Larry Liu <l_peng_l@qq.com>
2016-10-18 22:08:26 +0800

    使用Grunt构建工具

---
+++ slide-show/Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    concat: {
+      dist: {
+        src: ['src/js/slideShow.js'],
+        dest: 'dist/js/slideShow.js',
+      },
+    },
+    uglify: {
+      options: {
+        preserveComments: 'some',
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'dist/js/slideShow.min.js': ['src/js/slideShow.js']
+        }
+      }
+    },
+    cssmin: {
+      minify: {
+          src: 'dist/css/slideShow.css',
+          dest: 'dist/css/slideShow.min.css'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('build', ['concat', 'uglify', 'cssmin:minify']);
+};
